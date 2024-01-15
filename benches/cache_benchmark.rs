232f1a9efbbb63110ee66758c8b46dfa2ec9d3de
Matthias <26800596+not-matthias@users.noreply.github.com>
2024-01-16 00:51:33 +0100

    feat: add compare exchange weak

--- benches/cache_benchmark.rs
+++ benches/cache_benchmark.rs
@@ -1,16 +1,20 @@
-use atomic_cache_bench::cache_2;
-use atomic_cache_bench::{cache_1, cache_3, cache_4};
+use atomic_cache_bench::{compare_exchange, compare_exchange_weak, load_store};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench_compare_exchange(c: &mut Criterion) {
-    c.bench_function("compare exchange 1", |b| b.iter(|| cache_1()));
-    c.bench_function("compare exchange 2", |b| b.iter(|| cache_2()));
+    c.bench_function("compare exchange 1", |b| b.iter(|| compare_exchange::cache_0()));
+    c.bench_function("compare exchange 2", |b| b.iter(|| compare_exchange::cache_1()));
+}
+
+fn bench_compare_exchange_weak(c: &mut Criterion) {
+    c.bench_function("compare exchange weak 1", |b| b.iter(|| compare_exchange_weak::cache_0()));
+    c.bench_function("compare exchange weak 2", |b| b.iter(|| compare_exchange_weak::cache_1()));
 }
 
 fn bench_load_store(c: &mut Criterion) {
-    c.bench_function("load store 1", |b| b.iter(|| cache_3()));
-    c.bench_function("load store 2", |b| b.iter(|| cache_4()));
+    c.bench_function("load store 1", |b| b.iter(|| load_store::cache_0()));
+    c.bench_function("load store 2", |b| b.iter(|| load_store::cache_1()));
 }
 
-criterion_group!(benches, bench_compare_exchange, bench_load_store);
+criterion_group!(benches, bench_compare_exchange, bench_compare_exchange_weak, bench_load_store);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -4,54 +4,92 @@ fn calc_value() -> u64 {
     42
 }
 
-pub fn cache_1() -> u64 {
-    static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
 
-    let _ =
-        CACHED_VALUE.compare_exchange(u64::MAX, calc_value(), Ordering::Relaxed, Ordering::Relaxed);
+pub mod compare_exchange {
+    use super::*;
 
-    CACHED_VALUE.load(Ordering::Relaxed)
-}
+    pub fn cache_0() -> u64 {
+        static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
 
-pub fn cache_2() -> u64 {
-    static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+        let _ =
+            CACHED_VALUE.compare_exchange(u64::MAX, calc_value(), Ordering::Relaxed, Ordering::Relaxed);
 
-    let calc_value = calc_value();
-    match CACHED_VALUE.compare_exchange(u64::MAX, calc_value, Ordering::Relaxed, Ordering::Relaxed)
-    {
-        Ok(_) => {
-            // Write was successful
-            calc_value
+        CACHED_VALUE.load(Ordering::Relaxed)
+    }
+
+    pub fn cache_1() -> u64 {
+        static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+
+        let calc_value = calc_value();
+        match CACHED_VALUE.compare_exchange(u64::MAX, calc_value, Ordering::Relaxed, Ordering::Relaxed)
+        {
+            Ok(_) => {
+                // Write was successful
+                calc_value
+            }
+            Err(value) => value,
         }
-        Err(value) => value,
     }
 }
 
-pub fn cache_3() -> u64 {
-    static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+pub mod compare_exchange_weak {
+    use super::*;
 
-    let value = CACHED_VALUE.load(Ordering::Relaxed);
-    if value != u64::MAX {
-        return value;
+
+    pub fn cache_0() -> u64 {
+        static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+
+        let _ =
+            CACHED_VALUE.compare_exchange_weak(u64::MAX, calc_value(), Ordering::Relaxed, Ordering::Relaxed);
+
+        CACHED_VALUE.load(Ordering::Relaxed)
     }
 
-    let value = calc_value();
-    CACHED_VALUE.store(value, Ordering::Relaxed);
-    value
+    pub fn cache_1() -> u64 {
+        static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+
+        let calc_value = calc_value();
+        match CACHED_VALUE.compare_exchange_weak(u64::MAX, calc_value, Ordering::Relaxed, Ordering::Relaxed)
+        {
+            Ok(_) => {
+                // Write was successful
+                calc_value
+            }
+            Err(value) => value,
+        }
+    }
 }
 
-pub fn cache_4() -> u64 {
-    static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+pub mod load_store {
+    use super::*;
 
-    let value = CACHED_VALUE.load(Ordering::SeqCst);
-    if value != u64::MAX {
-        return value;
+    pub fn cache_0() -> u64 {
+        static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+
+        let value = CACHED_VALUE.load(Ordering::Relaxed);
+        if value != u64::MAX {
+            return value;
+        }
+
+        let value = calc_value();
+        CACHED_VALUE.store(value, Ordering::Relaxed);
+        value
     }
 
-    let value = calc_value();
-    CACHED_VALUE.store(value, Ordering::SeqCst);
-    value
+    pub fn cache_1() -> u64 {
+        static CACHED_VALUE: AtomicU64 = AtomicU64::new(u64::MAX);
+
+        let value = CACHED_VALUE.load(Ordering::SeqCst);
+        if value != u64::MAX {
+            return value;
+        }
+
+        let value = calc_value();
+        CACHED_VALUE.store(value, Ordering::SeqCst);
+        value
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -59,9 +97,11 @@ mod tests {
 
     #[test]
     fn test_values() {
-        assert_eq!(cache_1(), calc_value());
-        assert_eq!(cache_2(), calc_value());
-        assert_eq!(cache_3(), calc_value());
-        assert_eq!(cache_4(), calc_value());
+        assert_eq!(compare_exchange::cache_0(), calc_value());
+        assert_eq!(compare_exchange::cache_1(), calc_value());
+        assert_eq!(compare_exchange_weak::cache_0(), calc_value());
+        assert_eq!(compare_exchange_weak::cache_1(), calc_value());
+        assert_eq!(load_store::cache_0(), calc_value());
+        assert_eq!(load_store::cache_1(), calc_value());
     }
 }
